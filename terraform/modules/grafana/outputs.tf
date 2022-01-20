c4683b57970c50c0c6eb2d92c031a97d10f11cf0
TomJKing <tom.king@nationalarchives.gov.uk>
2022-01-20 08:08:26 +0000

    Run 'terraform fmt' command

--- terraform/modules/grafana/outputs.tf
+++ terraform/modules/grafana/outputs.tf
@@ -1,4 +1,4 @@
-output alb_security_group_id {
+output "alb_security_group_id" {
   value = aws_security_group.grafana_alb_group.id
 }
 
--- terraform/root_locals.tf
+++ terraform/root_locals.tf
@@ -1,11 +1,11 @@
 locals {
   aws_region = var.default_aws_region
   common_tags = tomap({
-    "Environment" =  local.environment,
-    "Owner" = "TDR",
-    "Terraform" = true,
+    "Environment"     = local.environment,
+    "Owner"           = "TDR",
+    "Terraform"       = true,
     "TerraformSource" = "https://github.com/nationalarchives/tdr-grafana/tree/master/terraform",
-    "CostCentre" = data.aws_ssm_parameter.cost_centre.value
+    "CostCentre"      = data.aws_ssm_parameter.cost_centre.value
   })
   database_availability_zones = ["eu-west-2a", "eu-west-2b"]
   environment                 = "mgmt"
